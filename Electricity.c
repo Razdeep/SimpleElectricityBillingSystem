daea9d969d48ce781a25e6fd76746ab38c78c9b5
razdeep <rrajdeeproychowdhury@gmail.com>
2017-11-17 20:24:04 +0530

    Version 3.1

--- Electricity.c
+++ Electricity.c
@@ -4,11 +4,7 @@
 #include<stdlib.h>
 #include<string.h>
 #include"rajdeep.h"
-/*#include<ctype.h>
-int strcmpi(char *str,char *temp)
-{
-	return strcmp(tolwr(str),tolwr(temp));
-}*/
+
 struct Record{
 	int id;
 	char name[30];
@@ -20,92 +16,8 @@ struct Record{
 };
 float first,second,third;
 void getch();
-void reviewDatabase()
-{
-	struct Record record[50];
-	struct Record tmp;
-	char temp[100];
-	int id,i,ch;
-	char name[30];
-	float first,second,third,units,amount;
-	FILE *fp=fopen("data.txt","r");
-	//Reading from file
-	for(i=0;;i++)
-	{
-		if(fscanf(fp,"%d%s%f%f%f%f%f",&record[i].id,record[i].name,&record[i].first,&record[i].second,&record[i].third,&record[i].units,&record[i].amount)==-1)
-		break;
-	}
-	int number=i;
-	printf("\n\t1. Sort by Name\n\t2. Sort by Amount or Units Consumed \n\t3. Sort by ID\n\t4. Sort by Input");
-	fflush(stdin);
-	printf("\nEnter Choice ");
-	scanf("%d",&ch);
-	printLine(30);
-	printf("\t\tDisplaying previously saved records... :)\n");
-	printLine(100);
-	printf("\nID\tName\t@1st 100 units\t@Next 100 Units\t@Remaining Units\tUnits Consumed\tAmount");
-	printLine(100);
-	/*while(fscanf(fp,"%d%s%f%f%f%f%f",&record.id,record.name,&record.first,&record.second,&record.third,&record.units,&record.amount)!=-1)
-	{
-		printf("\n%d\t%s\t%.2f\t\t%.2f\t\t%.2f\t\t\t%.2f\t\t%.2f",id,name,first,second,third,units,amount);
-	}*/
-	if(ch==1)
-	{
-		//Sorting by Name
-		for(int j=0;j<number-1;j++)
-		{
-		for(i=0;i<number-1;i++)
-		{
-				if(strcmp(record[i].name,record[i+1].name)==1)
-				{
-					tmp=record[i];
-					record[i]=record[i+1];
-					record[i+1]=tmp;
-				}
-			}
-		}
-	}
-	else if(ch==2)
-	{
-		//Sorting by Amount or Units Consumed
-		for(int j=0;j<number-1;j++)
-		{
-		for(i=0;i<number-1;i++)
-		{
-			if(record[i].amount>record[i+1].amount)
-			{
-				tmp=record[i];
-				record[i]=record[i+1];
-				record[i+1]=tmp;
-			}
-		}
-		}
-	}
-	else if(ch==3)
-	{
-		//sort by ID
-		for(int j=0;j<number-1;j++)
-		{
-		for(i=0;i<number-1;i++)
-		{
-			if(record[i].id>record[i+1].id)
-			{
-				tmp=record[i];
-				record[i]=record[i+1];
-				record[i+1]=tmp;
-			}
-		}
-		}
-	}
 
-	//printing the records list
-	for(i=0;i<number;i++)
-	{
-		printf("\n%d\t%s\t%.2f\t\t%.2f\t\t%.2f\t\t\t%.2f\t\t%.2f",record[i].id,record[i].name,record[i].first,record[i].second,record[i].third,record[i].units,record[i].amount);
-	}
 
-	getch();
-}
 void getch()
 {
 	int qwerty;
@@ -204,6 +116,184 @@ void setBillingParameters()
 		getch();
 	}
 }
+void editBill()
+{
+	loadParameters();
+	showParameters();
+	char ch;
+	struct Record record[50];
+	int i;
+	FILE *fp=fopen("data.txt","r");
+	//Reading from file
+	for(i=0;;i++)
+	{
+		if(fscanf(fp,"%d%s%f%f%f%f%f",&record[i].id,record[i].name,&record[i].first,&record[i].second,&record[i].third,&record[i].units,&record[i].amount)==-1)
+		break;
+	}
+	fclose(fp);
+	int number=i,flag=0;
+	//Display Records
+	printf("\t\tDisplaying previously saved records... :)\n");
+	printLine(100);
+	printf("\nID\tName\t@1st 100 units\t@Next 100 Units\t@Remaining Units\tUnits Consumed\tAmount");
+	printLine(100);
+
+	//printing the records list
+	for(i=0;i<number;i++)
+	{
+		printf("\n%d\t%s\t%.2f\t\t%.2f\t\t%.2f\t\t\t%.2f\t\t%.2f",record[i].id,record[i].name,record[i].first,record[i].second,record[i].third,record[i].units,record[i].amount);
+	}
+
+	int id;
+	printf("\nEnter ID of the bill which you want to edit ");
+	scanf("%d",&id);
+
+	for(i=0;i<number;i++)
+	{
+		if(record[i].id==id)
+		{
+			printf("\nEnter name ");
+			scanf("%s",record[i].name);
+			//level2:
+			fflush(stdin);
+			printf("\nEnter units of electricity consumed ");
+			scanf("%f",&record[i].units);
+			record[i].first=first;
+			record[i].second=second;
+			record[i].third=third;
+			/*if(record[i].units<0)
+			goto level2;*/
+			if(record[i].units>200)
+			{
+				record[i].amount=(100*first)+(100*second)+((record[i].units-200)*third);
+			}
+			else if(record[i].units>100 && record[i].units<=200)
+			{
+				record[i].amount=(100*first)+((record[i].units-100)*second);
+			}
+			else if(record[i].units<=100)
+			{
+				record[i].amount=record[i].units*first;
+			}
+			printf("Calculated Electricity Bill is Rs %.2f/-",record[i].amount);
+			flag=1;
+		}
+	}
+	if(flag==0)
+	{
+		printf("\nNo match found in our database.");
+	}
+	//Save
+	fflush(stdin);
+	printf("\nDO YOU WANT TO MAKE CHANGES TO THE DATABASE? (Y|N) ");
+	scanf(" %c",&ch);
+	flag=0;
+	if(ch=='Y'||ch=='y')
+	{
+		FILE *fp=fopen("data.txt","w");
+		for(i=0;i<number;i++)
+		{
+			if(fprintf(fp,"%d\t%s\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\n",record[i].id,record[i].name,record[i].first,record[i].second,record[i].third,record[i].units,record[i].amount)<0)
+			{
+				flag=1;
+				break;
+			}
+		}
+		fclose(fp);
+		if(flag==1)
+		{
+			printf("\nSorry for inconvenience. We are unable to modify our records. Please call Rajdeep.");
+		}
+		else{
+			printf("\nOur records have been successfully updated. Have a nice day. :) ");
+		}
+	}
+}
+void reviewDatabase()
+{
+	struct Record record[50];
+	struct Record tmp;
+	int i;
+	int ch;
+	FILE *fp=fopen("data.txt","r");
+	//Reading from file
+	for(i=0;;i++)
+	{
+		if(fscanf(fp,"%d%s%f%f%f%f%f",&record[i].id,record[i].name,&record[i].first,&record[i].second,&record[i].third,&record[i].units,&record[i].amount)==-1)
+		break;
+	}
+	fclose(fp);
+	int number=i;
+	printf("\n\t1. Sort by Name\n\t2. Sort by Amount or Units Consumed \n\t3. Sort by ID\n\t4. Sort by Input");
+	fflush(stdin);
+	printf("\nEnter Choice ");
+	scanf("%d",&ch);
+	printLine(30);
+	printf("\t\tDisplaying previously saved records... :)\n");
+	printLine(100);
+	printf("\nID\tName\t@1st 100 units\t@Next 100 Units\t@Remaining Units\tUnits Consumed\tAmount");
+	printLine(100);
+	/*while(fscanf(fp,"%d%s%f%f%f%f%f",&record.id,record.name,&record.first,&record.second,&record.third,&record.units,&record.amount)!=-1)
+	{
+		printf("\n%d\t%s\t%.2f\t\t%.2f\t\t%.2f\t\t\t%.2f\t\t%.2f",id,name,first,second,third,units,amount);
+	}*/
+	if(ch==1)
+	{
+		//Sorting by Name
+		for(int j=0;j<number-1;j++)
+		{
+		for(i=0;i<number-1;i++)
+		{
+				if(strcmp(record[i].name,record[i+1].name)==1)
+				{
+					tmp=record[i];
+					record[i]=record[i+1];
+					record[i+1]=tmp;
+				}
+			}
+		}
+	}
+	else if(ch==2)
+	{
+		//Sorting by Amount or Units Consumed
+		for(int j=0;j<number-1;j++)
+		{
+		for(i=0;i<number-1;i++)
+		{
+			if(record[i].amount>record[i+1].amount)
+			{
+				tmp=record[i];
+				record[i]=record[i+1];
+				record[i+1]=tmp;
+			}
+		}
+		}
+	}
+	else if(ch==3)
+	{
+		//sort by ID
+		for(int j=0;j<number-1;j++)
+		{
+		for(i=0;i<number-1;i++)
+		{
+			if(record[i].id>record[i+1].id)
+			{
+				tmp=record[i];
+				record[i]=record[i+1];
+				record[i+1]=tmp;
+			}
+		}
+		}
+	}
+
+	//printing the records list
+	for(i=0;i<number;i++)
+	{
+		printf("\n%d\t%s\t%.2f\t\t%.2f\t\t%.2f\t\t\t%.2f\t\t%.2f",record[i].id,record[i].name,record[i].first,record[i].second,record[i].third,record[i].units,record[i].amount);
+	}
+
+	getch();
+}
 void creditsAndTroubleshooting()
 {
 	printCredits();
@@ -230,7 +320,7 @@ void printMenu()
 		printf("\n");
 	}
 	printTitle("\tElectricity Billing System");
-	printf("1.\tCalculate Bill\n2.\tSet Billing Parameters\n3.\tCredits And Troubleshooting\n4.\tReview Database\n5.\tExit");
+	printf("1.\tCalculate Bill\n2.\tSet Billing Parameters\n3.\tEdit Bill from the database\n4.\tReview Database\n5.\tCredits And Troubleshooting\n6.\tExit");
 
 	printf("\n\nEnter Choice ");
 	scanf("%d",&ch);
@@ -243,14 +333,19 @@ void printMenu()
 		setBillingParameters();
 		break;
 		case 3:
-		creditsAndTroubleshooting();
+		editBill();
 		break;
 		case 4:
 		reviewDatabase();
 		break;
 		case 5:
+		creditsAndTroubleshooting();
+		break;
+		case 6:
+		creditsAndTroubleshooting();
 		exit(0);
 		break;
+
 		default:
 		choiceError();
 	}
